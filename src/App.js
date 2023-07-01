ba579bb6c8f9cad833d97a69bedff08ba6759f72
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-07-01 08:46:21 -0600

    created skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="outer-grid">
+      <div className="ouput">
+        <div className="operand"></div>
+        <div className="result"></div>
+      </div>
+      <div className="calc-buttons">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+        <button>÷</button>
+      </div>
     </div>
   );
 }
