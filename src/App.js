9f274d21fa74f33c55006a7d79c9fdcc020d2c63
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-07-03 12:53:22 -0600

    created the initial structure

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="outer-grid">
-      <div className="ouput">
-        <div className="operand"></div>
-        <div className="result"></div>
-      </div>
-      <div className="calc-buttons">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
+    <div className="calculator-body">
+      <div className="calculator-wrapper">     
+      <h1>Calculator</h1>
+      <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>&divide;</button>
         <button>1</button>
         <button>2</button>
         <button>3</button>
         <button>*</button>
-        <button>0</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
         <button>.</button>
-        <button>=</button>
-        <button>÷</button>
+        <button>0</button>
+        <button className="span-two">=</button>
       </div>
-    </div>
+      </div>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import "./Button.css";
+
+
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
